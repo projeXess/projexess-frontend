2c429d04f2f05926bbb5eaf5063d4f25426d7e5a
henritresor <shimwamanahenritresor@gmail.com>
2024-02-08 07:32:35 +0200

    initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ProjeXess",
+  description: "AI-based bilingual expert information, a structured framework, and market insights to help you succeed",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
