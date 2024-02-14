3fe819019484f14ca60e2557e861987b34a7fd5b
henritresor <shimwamanahenritresor@gmail.com>
2024-02-14 10:30:13 +0200

    created 404 page

---
+++ src/pages/(dashboard)/dashboard/404.tsx
@@ -0,0 +1,22 @@
+import { Link, useNavigate } from 'react-router-dom'
+
+
+function NotFound() {
+
+    const navigate = useNavigate()
+    return (
+        <div className="w-full h-full flex flex-col items-center justify-center">
+            <img
+                className="w-[30%] mb-11"
+                src="/404.svg"
+
+            />
+            <h1 className="text-[1.5rem] text-[#5c5c5c] mb-5">The Page You Visited Was Not Found</h1>
+            <Link to="#" onClick={() => navigate(-1)} className="text-[#3AA1FF]" >
+                go back
+            </Link>
+        </div>
+    )
+}
+
+export default NotFound
