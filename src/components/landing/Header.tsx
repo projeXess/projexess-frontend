fd77911ec913bc2cb869864a6a5017a8a7d4f20b
henritresor <shimwamanahenritresor@gmail.com>
2024-04-17 11:48:58 +0200

    created responsive aside bar

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -1,4 +1,6 @@
-import { Menu } from "lucide-react"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react'
+import { Menu, X } from "lucide-react"
 import { Link } from "react-router-dom"
 
 const navItems = [
@@ -8,14 +10,39 @@ const navItems = [
 ]
 
 function LandingHeader() {
+    const menuButton = React.useRef<HTMLButtonElement>(null)
+    const closeButton = React.useRef<HTMLButtonElement>(null)
+    const navItemsDiv = React.useRef<HTMLDivElement>(null)
+
+    React.useEffect(() => {
+        const handleClose = () => {
+            navItemsDiv.current?.classList.remove("active")
+        }
+        closeButton.current?.addEventListener('click', handleClose)
+
+        return () => closeButton.current?.removeEventListener('click', handleClose)
+    }, [])
+
+    React.useEffect(() => {
+
+        const handleOpenCloseMenu = () => {
+            navItemsDiv.current?.classList.add('active');
+        }
+        menuButton.current?.addEventListener("click", handleOpenCloseMenu)
+
+        return () => menuButton.current?.removeEventListener("click", handleOpenCloseMenu)
+    }, [])
 
     return (
         <div className="w-full flex p-2 justify-around shadow-lg top-0 sticky z-50 bg-white items-center">
-            <div className="flex items-center gap-3"> 
+            <div className="flex items-center gap-3">
                 <img src="/logo.svg" width={50} />
                 <h1 className="font-bold text-[#3AA1FF] md:text-[1.6rem] text-[1rem] ">ProjeXess</h1>
             </div>
-            <div className="hidden p-1 items-center gap-7 sm:flex md:text-[1rem] text-[0.8rem]">
+            <div className="hidden p-1 items-center gap-7 sm:flex md:text-[1rem] text-[0.8rem]" ref={navItemsDiv}>
+                <button className="btn-filled outline-none p-2 block sm:hidden" ref={closeButton}>
+                    <X />
+                </button>
                 {
                     navItems.map((item, index) => {
                         return (
@@ -25,9 +52,19 @@ function LandingHeader() {
                         )
                     })
                 }
+
+              
+                <div className="hidden items-center gap-3">
+                    <img src="/logo.svg" width={50} />
+                    <h1 className="font-bold text-[#3AA1FF] md:text-[1.6rem] text-[1rem] ">ProjeXess</h1>
+                </div>
+                <div className="items-center gap-5 hidden">
+                    <Link to={"/auth/login"}> <button className="btn-outlined p-2 md:text-[1rem] text-[0.9rem] rounded-lg">Login</button></Link>
+                    <Link to={"/auth/signup"}>  <button className="btn-filled p-2 md:text-[1rem] text-[0.9rem] rounded-lg ">Create account</button></Link>
+                </div>
             </div>
-            <button className="btn-filled outline-none p-2 block sm:hidden">
-                <Menu/>
+            <button className="btn-filled outline-none p-2 block sm:hidden" ref={menuButton}>
+                <Menu />
             </button>
             <div className="items-center gap-5 hidden sm:flex">
                 <Link to={"/auth/login"}> <button className="btn-outlined p-2 md:text-[1rem] text-[0.9rem] rounded-lg">Login</button></Link>
