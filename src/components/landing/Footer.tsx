3e6b39d2c677881208994e0647e368feb124d7b3
henritresor <shimwamanahenritresor@gmail.com>
2024-04-17 11:28:42 +0200

    added responsiveness to the landing page

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -8,16 +8,17 @@ import { Input } from "../ui/input"
 function LandingFooter() {
     return (
         <div className="w-full bg-white rounded-t-2xl h-auto mt-5 pb-10">
-            <div className="w-full p-5 mb-10">
-                <h1 className="font-bold text-[1.5rem] ">LOGO</h1>
+            <div className="w-full p-5 mb-10 flex items-center gap-3">
+                <img src="/logo.svg" width={50}  />
+                <h1 className="font-bold text-[1.5rem] ">Projexess</h1>
             </div>
 
-            <div className="flex p-1 w-full justify-around">
+            <div className="sm:flex sm:p-1 w-full justify-around grid grid-cols-2  p-5 gap-5">
                 <div className="flex flex-col">
                     <h1 className="mb-5 text-[1.2rem]">Explorer</h1>
                     {
                         explorerListItems.map((item, index) => {
-                            return <Link className="flex items-center gap-2 mt-5 text-[#5c5c5c] text-[.9rem]" to={item.link} key={index}>
+                            return <Link className="flex items-center gap-2 mt-5 text-[#5c5c5c] md:text-[1rem] text-[.7rem]" to={item.link} key={index}>
                                 {item.name} <span className="text-[15px] "><ArrowUpRight className="size-4" /></span>
                             </Link>
                         })
@@ -27,7 +28,7 @@ function LandingFooter() {
                     <h1 className="mb-5 text-[1.2rem]">Use Cases</h1>
                     {
                         useCasesItems.map((item, index) => {
-                            return <Link className="flex items-center gap-2 mt-5 text-[#5c5c5c] text-[.9rem]" to={item.link} key={index}>
+                            return <Link className="flex items-center gap-2 mt-5 text-[#5c5c5c] md:text-[1rem] text-[.7rem]" to={item.link} key={index}>
                                 {item.name} <span className="text-[15px] "><ArrowUpRight className="size-4" /></span>
                             </Link>
                         })
@@ -37,7 +38,7 @@ function LandingFooter() {
                     <h1 className="mb-5 text-[1.2rem]">Value Proposition</h1>
                     {
                         valuePropoItems.map((item, index) => {
-                            return <Link className="flex items-center gap-2 mt-5 text-[#5c5c5c] text-[.9rem]" to={item.link} key={index}>
+                            return <Link className="flex items-center gap-2 mt-5 text-[#5c5c5c] md:text-[1rem] text-[.7rem]" to={item.link} key={index}>
                                 {item.name} <span className="text-[15px] "><ArrowUpRight className="size-4" /></span>
                             </Link>
                         })
@@ -60,7 +61,7 @@ function LandingFooter() {
                         {
                             socials.map((item, index) => {
                                 return (
-                                    <span onClick={() => window.open(item.link, 'blank')} key={index} className="bg-[#3AA1FF] text-white p-2 rounded-full cursor-pointer">
+                                    <span onClick={() => window.open(item.link, 'blank')} key={index} className="bg-[#3AA1FF] text-white sm:p-2 p-[5px] rounded-full cursor-pointer">
                                         {item.icon}
                                     </span>
                                 )
--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -1,3 +1,4 @@
+import { Menu } from "lucide-react"
 import { Link } from "react-router-dom"
 
 const navItems = [
@@ -9,24 +10,28 @@ const navItems = [
 function LandingHeader() {
 
     return (
-        <div className="w-full flex p-2 justify-around shadow-lg top-0 sticky z-50 bg-white">
-            <div>
-                <h1 className="font-bold text-[#3AA1FF] text-[1.6rem]">ProjeXess</h1>
+        <div className="w-full flex p-2 justify-around shadow-lg top-0 sticky z-50 bg-white items-center">
+            <div className="flex items-center gap-3"> 
+                <img src="/logo.svg" width={50} />
+                <h1 className="font-bold text-[#3AA1FF] md:text-[1.6rem] text-[1rem] ">ProjeXess</h1>
             </div>
-            <div className="flex items-center gap-5">
+            <div className="hidden p-1 items-center gap-7 sm:flex md:text-[1rem] text-[0.8rem]">
                 {
                     navItems.map((item, index) => {
                         return (
-                            <Link className="text-[1.01rem]" key={index} to={item.link}>
+                            <Link className="" key={index} to={item.link}>
                                 {item.name}
                             </Link>
                         )
                     })
                 }
             </div>
-            <div className="flex items-center gap-5">
-                <Link to={"/auth/login"}> <button className="btn-outlined p-2 rounded-lg">Login</button></Link>
-                <Link to={"/auth/signup"}>  <button className="btn-filled p-2 rounded-lg">Create account</button></Link>
+            <button className="btn-filled outline-none p-2 block sm:hidden">
+                <Menu/>
+            </button>
+            <div className="items-center gap-5 hidden sm:flex">
+                <Link to={"/auth/login"}> <button className="btn-outlined p-2 md:text-[1rem] text-[0.9rem] rounded-lg">Login</button></Link>
+                <Link to={"/auth/signup"}>  <button className="btn-filled p-2 md:text-[1rem] text-[0.9rem] rounded-lg ">Create account</button></Link>
             </div>
         </div>
     )
